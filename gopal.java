261eff0e508b0a5467ac25eb63d33c988494b28e
venusiddarth <venusiddarth@172.20.20.20>
2017-08-24 21:51:08 -0400

    btbbb

--- gopal.java
+++ gopal.java
@@ -1,5 +1,6 @@
 class First {
   public static void main(String[] arguments) {
     System.out.println("Let's do something using Java technology.");
+    System.out.println("Let's do something using Java technology.");
   }
 }
